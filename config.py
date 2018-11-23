4d2b7369f80a601528b5b39cd5b3b24aa335cb06
Corey Sather <manicmachine@users.noreply.github.com>
2018-11-23 12:13:22 -0600

    Commit redacted config file
    Removed database URL & API keys

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ['FLASK_APP'] = "app.py"
+os.environ['FLASK_DEBUG'] = "1"
+os.environ['DATABASE_URL'] = ""
+os.environ['GOODREADS_KEY'] = ""
+os.environ['GOODREADS_SECRET'] = ""
